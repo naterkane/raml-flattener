cb7d3f9c200e115a9ac80abd697c1f23c6eca130
Nater Kane <nater@naterkane.com>
2015-04-06 12:39:00 -0400

    currently hanging on index.js after the loop on line 34 is run

---
+++ cli.js
@@ -0,0 +1,11 @@
+var cli = require('cli');
+var flattener = require('index.js');
+
+cli.parse({
+  filein: ['-i', 'Raml file to be parsed', 'file', './file.raml'],
+  fileout: ['-o', 'Name of flattened Raml file to be written', 'file', './file-flat.raml']
+
+});
+process.argv.forEach(function(val, index, array) {
+  console.log(index + ': ' + val);
+});
--- index.js
+++ index.js
@@ -5,38 +5,47 @@ var raml = require('raml-parser');
 var _ = require('lodash');
 var fs = require('fs');
 
-cli.parse({
-  filein: ['-i', 'Raml file to be parsed', 'file', './file.raml'],
-  fileout: ['-o', 'Name of flattened Raml file to be written', 'file', './file-flat.raml']
 
-});
-process.argv.forEach(function(val, index, array) {
-  console.log(index + ': ' + val);
-});
-console.log(cli.args);
-if (cli.args.length < 1){ return new Error('you must supply a .raml file for parsing'); }
-cli.main(function(args, options){
-  console.log("pooo" +args);
-  raml.composeFile(args.shift()).then(function(rootNode) {
+module.exports = Flattener;
 
+function Flattener(infile, outfile){
+
+  if (!infile){ return new Error('you must supply a .raml file for parsing'); }
+  /**
+   * generate output path if unspecified
+   */
+  if (!outfile) {
+    outfile = infile.split('/');
+    outfile = outfile.join('/').split('.').join('-flat.');
+  }
+  /**
+   * generate basepath
+   */
+  var basepath = infile.split('/').shift() + '/',
+      output;
+  console.log('importing ' + infile + ' and parsing to ' + outfile + ' with resources from ' + basepath);
+
+  raml.composeFile(infile).then(function(rootNode) {
     var doclines = rootNode.start_mark.buffer.split('\n'),
         newdoc = [],
         c = 0,
         msg = '';
+
     _.forOwn(doclines,function(line) {
       c++;
       var include,
           buffer,
           includelines = [],
           d = 0;
-
+      //console.log(line);
       if (line.match(/\!include\s(.*)/)) {
+        console.log(c + ' found a line');
         if (!msg){
           msg = 'importing external files for ' + doclines.length + ' lines';
           console.log(msg);
         }
         console.info('line ' + c + ': importing ' + line.match(/\!include\s(.*)/)[0].split(' ')[1]);
-        include = fs.readFileSync('raml/' + line.match(/\!include\s(.*)/)[0].split(' ')[1], 'utf8');
+        include = fs.readFileSync(basepath + line.match(/\!include\s(.*)/)[0].split(' ')[1], 'utf8');
         /**
          * let's maintain our indentation since raml is yaml-ish it's important
          */
@@ -44,7 +53,6 @@ cli.main(function(args, options){
         includelines = include.split('\n');
 
 
-        d = 0;
         _.forOwn(includelines, function(iline){
           includelines[d++] = buffer + iline;//iline.replace(/^/,buffer);
         });
@@ -52,10 +60,14 @@ cli.main(function(args, options){
         line = line.replace(/\!include\s(.*)/, '|\n' + include);
 
       }
-      newdoc.push(line);
+      if(newdoc.push(line)){
+        console.log('pushed ' + (d+1) + ' line(s)');
+      }
     });
-    //if (fs.file)
-    fs.writeFile(options.fileout,newdoc.join('\n'), 'utf8',function(err) {
+    console.log('done loop');
+    var output = newdoc.join('\n');
+    console.log('output ' + output);
+    fs.writeFile(outfile,output, 'utf8',function(err) {
           if (err) {
             console.warn(err);
             //exit;
@@ -81,4 +93,6 @@ cli.main(function(args, options){
     console.error(error);
   });
 
-});
+}
+//test it here
+var flat = new Flattener('example/ecfs.raml');
